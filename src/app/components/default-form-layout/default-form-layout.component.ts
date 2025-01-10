07310e6fa7e7c4f9e75555db93ef09931414d8ed
GabeMike52 <gmiqueiascarneiro52@gmail.com>
2025-01-10 19:45:32 -0300

    feat: added more structure to the page, and some style

--- src/app/components/default-form-layout/default-form-layout.component.ts
+++ src/app/components/default-form-layout/default-form-layout.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-default-form-layout',
-  imports: [],
-  templateUrl: './default-form-layout.component.html',
-  styleUrl: './default-form-layout.component.scss'
+    selector: 'app-default-form-layout',
+    imports: [],
+    templateUrl: './default-form-layout.component.html',
+    styleUrl: './default-form-layout.component.scss',
 })
 export class DefaultFormLayoutComponent {
-
+    @Input() title: string = '';
+    @Input() primaryBtnText: string = '';
+    @Input() secondaryBtnText: string = '';
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 import { DefaultFormLayoutComponent } from '../../components/default-form-layout/default-form-layout.component';
+import {
+    FormControl,
+    FormGroup,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
 
 @Component({
     selector: 'app-login',
-    imports: [DefaultFormLayoutComponent],
+    imports: [DefaultFormLayoutComponent, ReactiveFormsModule],
     templateUrl: './login.component.html',
     styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+    loginForm!: FormGroup;
+
+    constructor() {
+        this.loginForm = new FormGroup({
+            name: new FormControl('', [Validators.required]),
+            password: new FormControl('', [
+                Validators.required,
+                Validators.minLength(8),
+            ]),
+        });
+    }
+}
