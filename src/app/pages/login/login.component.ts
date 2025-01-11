4cb4d325049cfbbb655f61256ff24d002c33945d
GabeMike52 <gmiqueiascarneiro52@gmail.com>
2025-01-10 22:21:43 -0300

    feat: fixed some minor issues, finally changed the favicon, tested with the backend

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -9,6 +9,7 @@ import {
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
 
 interface LoginForm {
     username: FormControl;
@@ -29,7 +30,11 @@ interface LoginForm {
 export class LoginComponent {
     loginForm!: FormGroup<LoginForm>;
 
-    constructor(private router: Router, private loginService: LoginService) {
+    constructor(
+        private router: Router,
+        private loginService: LoginService,
+        private toastService: ToastrService
+    ) {
         this.loginForm = new FormGroup({
             username: new FormControl('', [Validators.required]),
             password: new FormControl('', [
@@ -43,8 +48,11 @@ export class LoginComponent {
         this.loginService
             .login(this.loginForm.value.username, this.loginForm.value.password)
             .subscribe({
-                next: () => console.log('Success'),
-                error: () => console.error('Error'),
+                next: () => this.toastService.success('Signed in successfully'),
+                error: () =>
+                    this.toastService.error(
+                        'Unexpected Error! Try again later'
+                    ),
             });
     }
 
