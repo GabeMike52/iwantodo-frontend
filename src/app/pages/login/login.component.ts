0973b00604309acc8138bd70ec4f820f754fec22
GabeMike52 <gmiqueiascarneiro52@gmail.com>
2025-01-10 21:26:04 -0300

    feat: added signin logic, still needs more work. added login service, data is being sent to the backend...

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -6,23 +6,49 @@ import {
     ReactiveFormsModule,
     Validators,
 } from '@angular/forms';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+
+interface LoginForm {
+    username: FormControl;
+    password: FormControl;
+}
 
 @Component({
     selector: 'app-login',
-    imports: [DefaultFormLayoutComponent, ReactiveFormsModule],
+    imports: [
+        DefaultFormLayoutComponent,
+        ReactiveFormsModule,
+        PrimaryInputComponent,
+    ],
+    providers: [LoginService],
     templateUrl: './login.component.html',
     styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-    loginForm!: FormGroup;
+    loginForm!: FormGroup<LoginForm>;
 
-    constructor() {
+    constructor(private router: Router, private loginService: LoginService) {
         this.loginForm = new FormGroup({
-            name: new FormControl('', [Validators.required]),
+            username: new FormControl('', [Validators.required]),
             password: new FormControl('', [
                 Validators.required,
                 Validators.minLength(8),
             ]),
         });
+    }
+
+    submit() {
+        this.loginService
+            .login(this.loginForm.value.username, this.loginForm.value.password)
+            .subscribe({
+                next: () => console.log('Success'),
+                error: () => console.error('Error'),
+            });
+    }
+
+    navigate() {
+        this.router.navigate(['signup']);
     }
 }
